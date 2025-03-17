e40bfa5a9d14d4b38639e70c351306e19dcafb49
Kou-ding <fotispapadak@gmail.com>
2025-03-17 17:32:39 +0200

    payoff matrix

--- main.m
+++ main.m
@@ -1,6 +1,9 @@
 function PrisonersDilemma()
-    PayoffMatrix = [3 4; 
-                    1 2];
+    % Fill in the payoffs
+    payoffMatrix{1, 1} = [3, 2]; % Player 1 chooses X, Player 2 chooses A
+    payoffMatrix{1, 2} = [0, 1]; % Player 1 chooses X, Player 2 chooses B
+    payoffMatrix{2, 1} = [1, 4]; % Player 1 chooses Y, Player 2 chooses A
+    payoffMatrix{2, 2} = [2, 3]; % Player 1 chooses Y, Player 2 chooses B
     
 end
 
