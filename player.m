9b86bbf4e14e8ea0827037a18a9b31d6e0308e64
Kou-ding <fotispapadak@gmail.com>
2025-03-21 01:02:11 +0200

    Add history property to player class for behavior tracking

--- player.m
+++ player.m
@@ -1,7 +1,8 @@
 classdef player 
     % Class's Properties
     properties
-        score = 0;
+        score = 0; % Player's score
+        history = behaviour.empty; % Empty array of behaviours
     end
 
     % Class's Methods
@@ -9,6 +10,7 @@ classdef player
         % Constructor
         function obj = player()
             obj.score = 0;
+            obj.history = behaviour.empty;
         end
         % Method to add score
         function obj = addScore(obj, score)
