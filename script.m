ed00c2f41614cff885e1f0d4a32e5f9084a238ee
Kou-ding <fotispapadak@gmail.com>
2025-03-28 19:07:46 +0200

    Initial parameter configuration script

---
+++ script.m
@@ -0,0 +1,10 @@
+% Strategy correspondance
+% 1: Random
+% 2: Cooperate
+% 3: Defect
+% 4: Tit-for-tat
+% 5: Grim
+strategiesArray = [2, 3, 4]; % Strategy numbers
+populationsArray = [10, 10, 10]; % Population sizes
+matrix = [3, 1; 4, 2]; % Payoff matrix
+rounds = 1; % Number of rounds
