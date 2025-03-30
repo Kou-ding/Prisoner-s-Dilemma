bb528fe1f2e1f606d3763166b121b9fac291b6a8
snazlidi <socratisnazlidis@protonmail.com>
2025-03-30 11:00:54 +0300

    Easy ones...
    Three easy starters...

---
+++ strategies/mistrust.m
@@ -0,0 +1,20 @@
+classdef mistrust < player
+    % Mistrust's methods
+    methods
+        % Constructor
+        function obj = mistrust()
+            obj@player();
+        end
+
+        % Mistrust: Defect on first move, then play what the opponent played on the previous move.
+        function obj = setMove(obj, opponentsLastMove, currentRound)
+
+            if(currentRound == 1)
+                obj.move = 1;
+            else
+                obj.move = opponentsLastMove;
+            end
+        end
+    end
+end
+
---
+++ strategies/pavlov.m
@@ -0,0 +1,22 @@
+classdef pavlov < player
+    % Mistrust's methods
+    methods
+         % Constructor
+        function obj = pavlov()
+            obj@player();
+        end
+         % Pavlov:  Cooperates on the first move, then cooperates only if the two players made the same move.
+        function obj = setMove(obj, opponentsLastMove, currentRound)
+
+            if(currentRound == 1)
+                obj.move = 0;
+            else
+                if obj.move == opponentsLastMove;
+                    obj.move = 0;
+                else 
+                    obj.move = 1;
+                end
+            end
+        end
+    end
+end
