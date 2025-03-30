2c5de81d8ea2f4ac9c5aa97fd6b225484788d4ac
snazlidi <socratisnazlidis@protonmail.com>
2025-03-30 11:07:49 +0300

    fix's fix...

--- strategies/spite.m
+++ strategies/spite.m
@@ -1,19 +1,19 @@
 classdef spite < player
-    % Mistrust's methods
+    % Spite's methods
     methods
          % Constructor
         function obj = spite()
             obj@player();
         end
          % Spite: Cooperates until the opponent defects, then defects all the time.
-        function obj = setMove(obj, opponentsLastMove, currentRound)
+        function obj = setMove(obj, opponentsLastMove, ~)
 
             while (opponentsLastMove == 0)
                 obj.move = 0;
             end
 
             obj.move = 1;
-            
+
         end
     end
 end
