366e9051485f3ff742897e87b994c2a3b5a20ee1
snazlidi <socratisnazlidis@protonmail.com>
2025-04-03 15:10:15 +0300

    periodics

---
+++ strategies/per_CD.m
@@ -0,0 +1,23 @@
+classdef per_CD < player
+
+    properties
+        internalState = 0; % 0: start or after D, 1: after C
+    end
+
+    methods
+        % Constructor
+        function obj = per_CD()
+            obj@player();
+        end
+
+        function obj = setMove(obj, ~, ~)
+            if obj.internalState == 0
+                obj.move = 0; % Cooperate
+                obj.internalState = 1;
+            else
+                obj.move = 1; % Defect
+                obj.internalState = 0;
+            end
+        end
+    end
+end
---
+++ strategies/per_kind.m
@@ -0,0 +1,28 @@
+classdef per_kind < player
+  
+    properties
+      state = 0; % 0: fresh start, 1: first C, 2: second C, 3: First D
+    end 
+  
+  methods 
+      % Constructor
+      function obj = per_kind()
+          obj@player();
+      end
+  
+      function obj = setMove(obj,~,~)
+          switch obj.stste
+              case 0
+                  obj.move = 0; % Begin by cooperating 
+                  obj.state = 1;
+              case 1
+                  obj.move = 0; % Second cooperation
+                  obj.state = 2;
+              case 2
+                  obj.move = 1; % Now defect
+                  obj.state = 0;
+          end
+      end
+  end
+  
+end 
