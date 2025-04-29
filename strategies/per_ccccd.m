d96fc4eed3d962ed3d3186c6517b96f2f7e8683e
Kou-ding <fotispapadak@gmail.com>
2025-04-30 01:25:14 +0300

    per_ccccd strategy implementation

---
+++ strategies/per_ccccd.m
@@ -0,0 +1,35 @@
+% Periodically plays cooperate,defect
+classdef per_ccccd < player
+
+    properties
+        state = []; % 0: start or after D, 1: after C
+    end
+
+    methods
+        % Constructor
+        function obj = per_ccccd(numberOfPlayers)
+            obj@player();
+            obj.state(numberOfPlayers, 1) = 0;
+        end
+
+        function obj = setMove(obj, ~, opponentIndex, ~)
+            switch obj.state(opponentIndex, 1)
+                case 0
+                      obj.move = 0; % Begin by cooperating 
+                      obj.state(opponentIndex, 1) = 1;
+                case 1
+                      obj.move = 0; % Second cooperation
+                      obj.state(opponentIndex, 1) = 2;
+                case 2
+                      obj.move = 0; % Third cooperation
+                      obj.state(opponentIndex, 1) = 3;
+                case 3
+                      obj.move = 0; % Fourth cooperation
+                      obj.state(opponentIndex, 1) = 4;
+                case 4
+                      obj.move = 1; % Now defect
+                      obj.state(opponentIndex, 1) = 0;
+            end
+        end
+    end
+end
