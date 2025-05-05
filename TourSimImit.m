212fb551ab6658f89a0c6ae7c74d6589270c62d7
Kou-ding <fotispapadak@gmail.com>
2025-05-06 00:14:38 +0300

    remove unnecessary whitespace in TourSimFit and TourSimImit functions

--- TourSimImit.m
+++ TourSimImit.m
@@ -1,5 +1,4 @@
-function TourSimImit(matrix, strategiesArray, populationsArray, K , rounds, generations)
-    
+function TourSimImit(matrix, strategiesArray, populationsArray, K , rounds, generations) 
     tournament = genaxel();
     tournament.callTourSimImi(matrix, strategiesArray, populationsArray, K , rounds, generations);
 end
