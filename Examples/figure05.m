4a96b2f9e56639ef5e0efbdc70d82db6d8f66877
Kou-ding <fotispapadak@gmail.com>
2025-05-29 02:05:27 +0300

    Fix strategiesArray values in figure05.m for correct oscillation representation

--- Examples/figure05.m
+++ Examples/figure05.m
@@ -1,5 +1,5 @@
 % Increasing oscillations X
-strategiesArray = [9, 11, 12]; % soft_majo, per_ccd, per_ddc
+strategiesArray = [9, 10, 12]; % soft_majo, per_cd, per_ddc
 populationsArray = [200, 300, 400];
 matrix = [3, 0; 5,1];
 rounds = 1000;
